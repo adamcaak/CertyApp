894e085714d9038a2cd85375654d925287da4ead
adamcaak <ladamczak1997@gmail.com>
2025-10-14 18:49:39 +0200

    Create new file Certificate with class and init

---
+++ CertyApp/Models/Certificate.swift
@@ -0,0 +1,28 @@
+//
+//  Certificate.swift
+//  CertyApp
+//
+//  Created by Łukasz Adamczak on 14/10/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Certificate {
+    @Attribute(.unique) var id: UUID = UUID()
+    var title: String
+    var platform: String
+    var date: Date
+    var category: String
+    var imageData: Data?
+    
+    init(id: UUID, title: String, platform: String, date: Date, category: String, imageData: Data? = nil) {
+        self.id = id
+        self.title = title
+        self.platform = platform
+        self.date = date
+        self.category = category
+        self.imageData = imageData
+    }
+}
