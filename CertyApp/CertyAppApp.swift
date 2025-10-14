57c4fd6929a3fbc17595c888e83b0a0348098aa6
adamcaak <ladamczak1997@gmail.com>
2025-10-14 18:38:55 +0200

    Initial Commit

---
+++ CertyApp/CertyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertyAppApp.swift
+//  CertyApp
+//
+//  Created by Łukasz Adamczak on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct CertyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
